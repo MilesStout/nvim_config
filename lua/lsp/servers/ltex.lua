911615fc7a9fc79bb2d49e807ee1c1c0d2192544
Miles Stout <milesmstout@gmail.com>
2026-07-19 18:40:59 -0400

    fixed filetype bug

--- lua/lsp/servers/ltex.lua
+++ lua/lsp/servers/ltex.lua
@@ -1,6 +1,6 @@
 return {
   cmd = { "ltex-ls" },
-  filetypes = { "tex", "markdown", "plaintex", "bib", "gitcommit", "org", "rst", "latex", "quarto", "rmd", "context", "html", "mail", "text", "xhtml", "rnoweb", "pandoc", "plaintex" },
+  filetypes = { "tex", "markdown", "plaintex", "bib", "gitcommit", "org", "rst", "quarto", "rmd", "context", "html", "mail", "text", "xhtml", "rnoweb", "pandoc", "plaintex" },
   settings = {
     ltex = {
       enabled = { "bibtex", "gitcommit", "markdown", "org", "tex", "restructuredtext", "rsweave", "latex", "quarto", "rmd", "context", "html", "xhtml", "mail", "plaintext" },
