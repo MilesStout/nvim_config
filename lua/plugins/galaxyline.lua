f2712c2c6c3a0fbb2c09898303eb9edc139b1911
Miles Stout <milesmstout@gmail.com>
2026-07-19 19:39:13 -0400

    changed from galaxyline to lualine

--- lua/plugins/galaxyline.lua
+++
@@ -1,9 +0,0 @@
-return{
-	"NTBBloodbath/galaxyline.nvim",
-
-	dependencies = {'kyazdani42/nvim-web-devicons', opt = true}, 
-	
-	config = function()
-		require('galaxyline.themes.spaceline')
-	end,
-}
