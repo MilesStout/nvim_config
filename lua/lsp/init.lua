15d3d608b289661eec2b11466705325918259bf7
Miles Stout <milesmstout@gmail.com>
2026-08-03 09:06:08 -0400

    continuing to work on native lsp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -4,52 +4,45 @@ function M.setup()
 
   local servers = { "pyright", "lua_ls", "bashls", "ltex", "jdtls" }
 
-
   vim.opt.complete = ".,o"
-  vim.opt.completeopt = { "fuzzy", "menuone", "noselect", "popup"}
+  vim.opt.completeopt = { "fuzzy", "menuone", "noselect", "popup" }
   vim.opt.autocomplete = true
-  
 
   for _, name in ipairs(servers) do
 
     local config = require("lsp.servers." .. name)
+
     vim.lsp.config(name, config)
     vim.lsp.enable(name)
 
   end
 
-  vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, {
-      -- Optional formating of items
-      convert = function(item)
-        -- Remove leading misc chars for abbr name,
-        -- and cap field to 25 chars
-        --local abbr = item.label
-        --abbr = abbr:match("[%w_.]+.*") or abbr
-        --abbr = #abbr > 25 and abbr:sub(1, 24) .. "…" or abbr
-        --
-        -- Remove return value
-        --local menu = ""
+vim.api.nvim_create_autocmd("LspAttach", {
 
-        -- Only show abbr name, remove leading misc chars (bullets etc.),
-        -- and cap field to 15 chars
+  callback = function(ev)
+
+    vim.api.nvim_buf_set_option(ev.buf, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, {
+
+      convert = function(item)
+
         local abbr = item.label
+
         abbr = abbr:gsub("%b()", ""):gsub("%b{}", "")
         abbr = abbr:match("[%w_.]+.*") or abbr
         abbr = #abbr > 15 and abbr:sub(1, 14) .. "…" or abbr
 
-        -- Cap return value field to 15 chars
         local menu = item.detail or ""
+
         menu = #menu > 15 and menu:sub(1, 14) .. "…" or menu
 
         return { abbr = abbr, menu = menu }
+
       end,
     })
   end,
 })
-
 end
-
 
 return M
